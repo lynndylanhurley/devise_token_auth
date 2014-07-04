32273de16ab3ec91d3154507ab00ce44095497ae
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2014-07-04 18:43:49 -0500

    set user to confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable
+        :recoverable, :rememberable, :trackable, :validatable,
+        :confirmable
 
   serialize :tokens, JSON
 
