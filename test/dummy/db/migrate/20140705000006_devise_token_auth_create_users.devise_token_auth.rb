d232580a00f6a0ccaf89a0d7f2baa59c6c6e7483
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2014-07-06 17:29:50 -0500

    remove unique constraint from user email

--- test/dummy/db/migrate/20140705000006_devise_token_auth_create_users.devise_token_auth.rb
+++ test/dummy/db/migrate/20140705000006_devise_token_auth_create_users.devise_token_auth.rb
@@ -47,8 +47,8 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
+    add_index :users, :email
     add_index :users, :uid,                  :unique => true
-    add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
