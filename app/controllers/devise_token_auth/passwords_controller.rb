eecd6f0c53991fbcd4420bf76f8205c155b938d3
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2014-07-13 18:20:36 -0500

    password reset [wip]

--- app/controllers/devise_token_auth/passwords_controller.rb
+++ app/controllers/devise_token_auth/passwords_controller.rb
@@ -1,53 +1,70 @@
 module DeviseTokenAuth
-  class PasswordsController < Devise::PasswordsController
+  class PasswordsController < DeviseTokenAuth::ApplicationController
     include Devise::Controllers::Helpers
+    skip_before_filter :set_user_by_token, :only => [:create]
+    skip_after_filter :update_auth_header, :only => [:create]
 
     def create
-      self.resource = resource_class.send_reset_password_instructions(resource_params)
-      yield resource if block_given?
+      # send the confirmation email
+      @user = User.send_reset_password_instructions({
+        email: resource_params[:email],
+        provider: 'email'
+      })
 
-      throw "Not implemented"
-
-      if resource.errors.empty?
+      if @user.errors.empty?
         render json: {
-          success: true 
+          success: true,
+          message: "An email has been sent to #{@user.email} containing "+
+            "instructions for resetting your password."
         }
       else
         render json: {
           success: false,
-          errors: ["Something went wrong. Please contact support@healthbox.com."]
+          errors: @user.errors
         }, status: 401
       end
     end
 
 
     def update
-      self.resource = resource_class.reset_password_by_token(resource_params)
-      yield resource if block_given?
+      # make sure account doesn't use oauth2 provider
+      unless @user.provider == 'email'
+        render json: {
+          success: false,
+          errors: ["This account does not require a password. Sign in using "+
+                   "your #{@user.provider.humanize} account instead."]
+        }, status: 401
+      end
 
-      throw "Not implemented"
+      # ensure that password params were sent
+      unless resource_params[:password] and resource_params[:password_confirmaiton]
+        render json: {
+          success: false,
+          errors: ['You must fill out the fields labeled "password" and "password confirmation".']
+        }, status: 401
+      end
 
-      if resource.errors.empty?
-        resource.unlock_access! if unlockable?(resource)
+      @user.update_attributes(resource_params)
 
+      if @user.errors.empty?
         render json: {
           success: true,
           data: {
-            user: self.resource
+            user: @user,
+            message: "Your password has been successfully updated."
           }
         }
       else
         render json: {
           success: false,
-          errors: ["Something went wrong. Please contact support@healthbox.com."]
+          errors: @user.errors
         }, status: 401
       end
     end
 
 
     def resource_params
-      params.permit(:email, :password, :password_confirmation, :reset_password_token)
+      params.permit(:email, :password, :password_confirmation)
     end
-
   end
 end
