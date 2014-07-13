0baab8c2938fe4c1dd55d8c6c73cf7046361a1df
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2014-07-13 00:15:39 -0500

    re-migrate

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705000006) do
+ActiveRecord::Schema.define(version: 20140712225932) do
 
   create_table "users", force: true do |t|
     t.string   "email"
