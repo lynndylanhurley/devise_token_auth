b60c7c304c684e0fa7ee0a565f7f6b3b53913efd
Luis Edimerchk Laverde <edyhack@gmail.com>
2015-04-11 07:54:56 -0500

    URI fragment should appear at the end of URL

--- app/models/devise_token_auth/concerns/user.rb
+++ app/models/devise_token_auth/concerns/user.rb
@@ -212,16 +212,14 @@ module DeviseTokenAuth::Concerns::User
   protected
 
 
-  # NOTE: ensure that fragment comes AFTER querystring for proper $location
-  # parsing using AngularJS.
   def generate_url(url, params = {})
     uri = URI(url)
 
     res = "#{uri.scheme}://#{uri.host}"
     res += ":#{uri.port}" if (uri.port and uri.port != 80 and uri.port != 443)
     res += "#{uri.path}" if uri.path
-    res += "##{uri.fragment}" if uri.fragment
     res += "?#{params.to_query}"
+    res += "##{uri.fragment}" if uri.fragment
 
     return res
   end
