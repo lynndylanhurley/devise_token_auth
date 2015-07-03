eab1e7a28484fe2597b07ca693fa084059857e12
Nick Lewis <nlewisinc@gmail.com>
2015-07-03 12:11:57 -0500

    [bugfix]: User::Concern - Send password reset instructions only to confirmed email

--- app/models/devise_token_auth/concerns/user.rb
+++ app/models/devise_token_auth/concerns/user.rb
@@ -75,13 +75,6 @@ module DeviseTokenAuth::Concerns::User
 
       # fall back to "default" config name
       opts[:client_config] ||= "default"
-
-      if respond_to?(:pending_reconfirmation?) && pending_reconfirmation?
-        opts[:to] = unconfirmed_email
-      else
-        opts[:to] = email
-      end
-
       send_devise_notification(:reset_password_instructions, token, opts)
 
       token
