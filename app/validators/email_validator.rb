bccb7ef3b71c32ecb1d5f8f258353fa498e54e58
Yaroslav Konoplov <yaroslav@inbox.com>
2015-09-01 23:53:57 +0300

    Fallback to ActiveModel translations in EmailValidator

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,7 +1,25 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || I18n.t("errors.not_email"))
+      record.errors[attribute] << email_invalid_message
     end
   end
+  
+  private
+  
+  def email_invalid_message
+    # Try strictly set message:
+    message = options[:message]
+    
+    if message.nil?
+
+      # Try DeviceTokenAuth translations:
+      message = I18n.t('errors.not_email', default: '')
+
+      # Fallback to ActiveModel translations:
+      message = I18n.t('errors.messages.invalid') if message.blank?
+    end
+    
+    message
+  end  
 end
