408aa244a2dbcc6aef029ff029df468346412ebb
Nate Brustein <nbrustein@gmail.com>
2015-10-27 09:27:38 -0400

    fix(url): preserve query parameters when building urls

--- lib/devise_token_auth/url.rb
+++ lib/devise_token_auth/url.rb
@@ -5,8 +5,9 @@ module DeviseTokenAuth::Url
 
     res = "#{uri.scheme}://#{uri.host}"
     res += ":#{uri.port}" if (uri.port and uri.port != 80 and uri.port != 443)
-    res += "#{uri.path}" if uri.path
-    res += "?#{params.to_query}"
+    res += "#{uri.path}" if uri.path    
+    query = [uri.query, params.to_query].reject(&:blank?).join('&')
+    res += "?#{query}"
     res += "##{uri.fragment}" if uri.fragment
 
     return res
