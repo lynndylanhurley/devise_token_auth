f772ce6ddce14dae3fce41a5c7ec1092f7945762
cj <chirag7jain@gmail.com>
2015-11-07 12:06:32 +0530

    Device mapping duplicate method removed
    Since SetUserByToken concern already has the resource_class method and
    application controller is including the same concern, there is no need to
    keep the same duplicate method in application controller.

--- app/controllers/devise_token_auth/application_controller.rb
+++ app/controllers/devise_token_auth/application_controller.rb
@@ -1,17 +1,5 @@
 module DeviseTokenAuth
   class ApplicationController < DeviseController
     include DeviseTokenAuth::Concerns::SetUserByToken
-
-    protected
-
-    def resource_class(m=nil)
-      if m
-        mapping = Devise.mappings[m]
-      else
-        mapping = Devise.mappings[resource_name] || Devise.mappings.values.first
-      end
-
-      mapping.to
-    end
   end
 end
