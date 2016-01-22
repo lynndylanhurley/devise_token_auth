50c6d919ee9b980cbd0ac6cca6f23a3cecd0d2a6
djsegal <djsegal@wisc.edu>
2016-01-22 18:56:38 -0500

    Change before_filter to before_action for rails5

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -1,7 +1,7 @@
 # see http://www.emilsoman.com/blog/2013/05/18/building-a-tested/
 module DeviseTokenAuth
   class SessionsController < DeviseTokenAuth::ApplicationController
-    before_filter :set_user_by_token, :only => [:destroy]
+    before_action :set_user_by_token, :only => [:destroy]
     after_action :reset_session, :only => [:destroy]
 
     def new
