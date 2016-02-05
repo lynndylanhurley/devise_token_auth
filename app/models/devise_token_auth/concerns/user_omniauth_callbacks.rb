4a9df4f5b6451adefbffd3baeef54c757e3daad5
Alexander Merkulov <sasha@merqlove.ru>
2016-02-06 00:26:01 +0300

    Extract callbacks. Add setting for exctracted callbacks. Load it by default. Fix generator template. Add line to readme.

---
+++ app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
@@ -0,0 +1,28 @@
+module DeviseTokenAuth::Concerns::UserOmniauthCallbacks
+  extend ActiveSupport::Concern
+
+  included do
+    validates :email, presence: true, email: true, if: Proc.new { |u| u.provider == 'email' }
+    validates_presence_of :uid, if: Proc.new { |u| u.provider != 'email' }
+
+    # only validate unique emails among email registration users
+    validate :unique_email_user, on: :create
+
+    # keep uid in sync with email
+    before_save :sync_uid
+    before_create :sync_uid
+  end
+
+  protected
+
+  # only validate unique email among users that registered by email
+  def unique_email_user
+    if provider == 'email' and self.class.where(provider: 'email', email: email).count > 0
+      errors.add(:email, I18n.t("errors.messages.already_in_use"))
+    end
+  end
+
+  def sync_uid
+    self.uid = email if provider == 'email'
+  end
+end
