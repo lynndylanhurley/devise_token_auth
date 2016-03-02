f306631ce05a88983d7821790ebe3ede2a166f56
Charles De Groote <charles.degroote@gmail.com>
2016-03-02 15:29:33 +0100

    @resource.save! to have 400 error if validation failed.
    add a ! to the save to raise an ActiveRecord::RecordInvalid exception to have a 4** http code in the response if the validation failed.

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -38,7 +38,7 @@ module DeviseTokenAuth
           token: BCrypt::Password.create(@token),
           expiry: (Time.now + DeviseTokenAuth.token_lifespan).to_i
         }
-        @resource.save
+        @resource.save!
 
         sign_in(:user, @resource, store: false, bypass: false)
 
