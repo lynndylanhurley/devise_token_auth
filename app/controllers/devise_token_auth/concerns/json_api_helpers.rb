eb79f0e45a01d004826302d28a31f17d2c324940
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-03-08 23:18:46 +0100

    Set JSON API media type HTTP headers

--- app/controllers/devise_token_auth/concerns/json_api_helpers.rb
+++ app/controllers/devise_token_auth/concerns/json_api_helpers.rb
@@ -1,6 +1,19 @@
 module DeviseTokenAuth::Concerns::JsonApiHelpers
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_json_api_headers
+  end
 
   protected
+
+  # Sets the JSON API media type headers on the response object.
+  def set_json_api_headers
+    if configured_format == :json_api && response.present?
+      response.headers['HTTP_ACCEPT']   = 'application/vnd.api+json'
+      response.headers['CONTENT_TYPE']  = 'application/vnd.api+json'
+    end
+  end
 
   # Raises an ArgumentError when the `format` argument is unknown.
   def raise_unknown_format_argument_error
