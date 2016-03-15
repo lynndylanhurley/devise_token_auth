f7ea980b42408748295c7f021556a1f98fdadbdc
Olle Jonsson <olle.jonsson@ecraft.com>
2016-03-15 18:00:15 +0100

    Whitespace: tabs removed

--- test/lib/devise_token_auth/url_test.rb
+++ test/lib/devise_token_auth/url_test.rb
@@ -1,28 +1,24 @@
 require 'test_helper'
 
 class DeviseTokenAuth::UrlTest < ActiveSupport::TestCase
-	describe "DeviseTokenAuth::Url#generate" do
-	  test 'URI fragment should appear at the end of URL' do
-	    params = {client_id: 123}
-	    url = 'http://example.com#fragment'
-	    assert_equal DeviseTokenAuth::Url.send(:generate, url, params), "http://example.com?client_id=123#fragment"
-	  end
+  describe "DeviseTokenAuth::Url#generate" do
+    test 'URI fragment should appear at the end of URL' do
+      params = {client_id: 123}
+      url = 'http://example.com#fragment'
+      assert_equal DeviseTokenAuth::Url.send(:generate, url, params), "http://example.com?client_id=123#fragment"
+    end
 
-	  describe 'with existing query params' do
-	  	test 'should preserve existing query params' do
-	      url = 'http://example.com?a=1'
-	      assert_equal DeviseTokenAuth::Url.send(:generate, url), "http://example.com?a=1"
-	    end
+    describe 'with existing query params' do
+      test 'should preserve existing query params' do
+        url = 'http://example.com?a=1'
+        assert_equal DeviseTokenAuth::Url.send(:generate, url), "http://example.com?a=1"
+      end
 
-	    test 'should marge existing query params with new ones' do
-	      params = {client_id: 123}
-	      url = 'http://example.com?a=1'
-	      assert_equal DeviseTokenAuth::Url.send(:generate, url, params), "http://example.com?a=1&client_id=123"
-	    end
-
-
-	  end
-
-
-	end
+      test 'should marge existing query params with new ones' do
+        params = {client_id: 123}
+        url = 'http://example.com?a=1'
+        assert_equal DeviseTokenAuth::Url.send(:generate, url, params), "http://example.com?a=1&client_id=123"
+      end
+    end
+  end
 end
