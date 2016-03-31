8f638022c404c13c7b29748fa808e6d1a419287c
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-03-31 19:51:15 +0200

    Add helpers for dealing with JSON API compliant requests

--- app/controllers/devise_token_auth/concerns/json_api_helpers.rb
+++ app/controllers/devise_token_auth/concerns/json_api_helpers.rb
@@ -29,6 +29,40 @@ module DeviseTokenAuth::Concerns::JsonApiHelpers
     render json: content_hash, status: status
   end
 
+  def meta_params
+    case request_format
+    when :custom    # custom JSON request format
+      params
+    when :json_api  # JSON API specification compliant request format
+      json_api_meta_params
+    else
+      raise_unknown_format_argument_error
+    end
+  end
+
+  def data_attributes
+    case request_format
+    when :custom    # custom JSON request format
+      params
+    when :json_api  # JSON API specification compliant request format
+      json_api_data_attributes
+    else
+      raise_unknown_format_argument_error
+    end
+  end
+
+  def json_api_meta_params
+    ActionController::Parameters.new(params.require(:meta))
+  end
+
+  def json_api_data_params
+    ActionController::Parameters.new(params.require(:data))
+  end
+
+  def json_api_data_attributes
+    ActionController::Parameters.new(self.data_params.require(:attributes))
+  end
+
   # Sets the JSON API media type headers on the response object.
   def set_json_api_headers
     if response_format == :json_api && response.present?
@@ -41,9 +75,20 @@ module DeviseTokenAuth::Concerns::JsonApiHelpers
     raise ArgumentError, 'Unknown format argument specified', caller
   end
 
-  # Returns the response rendering format specified in the config.
+  # Returns the request format specified via config and request.
+  def request_format
+    format_for_http_header :content_type
+  end
+
+  # Returns the response rendering format specified via config and request.
   def response_format
-    accept = request.env['HTTP_ACCEPT'] if request.present?
+    format_for_http_header :accept
+  end
+
+  private
+
+  def format_for_http_header(header_key)
+    accept = request.env["HTTP_#{header_key.to_s.upcase}"] if request.present?
     if DeviseTokenAuth.json_api_enabled && accept.present? && accept.include?('application/vnd.api+json')
       :json_api
     else
