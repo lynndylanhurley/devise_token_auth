f5efd4fe9976ece4370ebca06da576b27dcf3a58
Daniel Neis Araujo <danielneis@gmail.com>
2017-09-01 18:55:02 -0300

    Allow multiple authentication_keys on login

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -10,26 +10,28 @@ module DeviseTokenAuth
 
     def create
       # Check
-      field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
+      fields = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys)
 
       @resource = nil
-      if field
-        q_value = resource_params[field]
+      if fields
+        conditions = {}
+        fields.each do |field|
+          q_value = resource_params[field]
 
-        if resource_class.case_insensitive_keys.include?(field)
-          q_value.downcase!
+          if resource_class.case_insensitive_keys.include?(field)
+            q_value.downcase!
+          end
+
+          q = "#{field.to_s} = ? AND provider='email'"
+
+          conditions[field] = q_value
         end
+        conditions[:provider] = 'email'
 
-        q = "#{field.to_s} = ? AND provider='email'"
-
-        if ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'mysql'
-          q = "BINARY " + q
-        end
-
-        @resource = resource_class.where(q, q_value).first
+        @resource = resource_class.where(conditions).first
       end
 
-      if @resource && valid_params?(field, q_value) && (!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
+      if @resource && (!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
         valid_password = @resource.valid_password?(resource_params[:password])
         if (@resource.respond_to?(:valid_for_authentication?) && !@resource.valid_for_authentication? { valid_password }) || !valid_password
           render_create_error_bad_credentials
