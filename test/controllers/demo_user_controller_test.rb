a04abfff24b2c5f591d96810b01b9c97fa3187d1
Lynn Hurley <lynn.dylan.hurley@gmail.com>
2017-10-13 13:37:54 -0500

    [bugfix] typo in tests. squash into previous commit

--- test/controllers/demo_user_controller_test.rb
+++ test/controllers/demo_user_controller_test.rb
@@ -263,7 +263,7 @@ class DemoUserControllerTest < ActionDispatch::IntegrationTest
           end
 
           it 'should not treat first request as batch request' do
-            refute @secord_is_batch_request
+            refute @second_is_batch_request
           end
 
           it 'should return auth headers from the first request' do
@@ -271,7 +271,7 @@ class DemoUserControllerTest < ActionDispatch::IntegrationTest
           end
 
           it 'should not treat second request as batch request' do
-            refute @secord_is_batch_request
+            refute @second_is_batch_request
           end
 
           it 'should not return auth headers from the second request' do
