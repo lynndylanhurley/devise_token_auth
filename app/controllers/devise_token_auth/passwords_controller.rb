05248b61826829ef6a16a8a264c28f520c34d8de
Maicol Bentancor <maicol@rootstrap.com>
2017-11-25 23:17:22 -0300

    removed unnecessary params from resource params (#1029)

--- app/controllers/devise_token_auth/passwords_controller.rb
+++ app/controllers/devise_token_auth/passwords_controller.rb
@@ -189,7 +189,7 @@ module DeviseTokenAuth
     private
 
     def resource_params
-      params.permit(:email, :password, :password_confirmation, :current_password, :reset_password_token, :redirect_url, :config)
+      params.permit(:email, :reset_password_token)
     end
 
     def password_resource_params
