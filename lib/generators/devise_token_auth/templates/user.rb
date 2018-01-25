1c79cfaddaee5731e4432ccff53c6ced06f9eb6a
Derek Kniffin <derek.kniffin@gmail.com>
2018-01-25 10:32:50 -0500

    Update user template (#1071)
    Moved confirmable and omniauthable to a comment

--- lib/generators/devise_token_auth/templates/user.rb
+++ lib/generators/devise_token_auth/templates/user.rb
@@ -1,7 +1,7 @@
 class <%= user_class.capitalize %> < ActiveRecord::Base
-  # Include default devise modules.
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
