d191828e0d241ba2533e65aa7e20fa72fb9d7f38
Evan Marell <evan.marell@gmail.com>
2018-03-23 19:11:36 -0700

    Fix `Style/SymbolArray`
     - Move `Style/SymbolArray` from todos to main config:
        * Set enforced style to 'percent' -- i.e. `%i[]`.
        * Don't enforce on arrays with < 3 elements.
    
     - Use percent array literals for large arrays of symbols.
    
     - Use percent array literals for large arrays of symbols.

--- test/dummy/app/controllers/overrides/token_validations_controller.rb
+++ test/dummy/app/controllers/overrides/token_validations_controller.rb
@@ -9,9 +9,7 @@ module Overrides
       if @resource
         render json: {
           success: true,
-          data: @resource.as_json(except: [
-            :tokens, :created_at, :updated_at
-          ]),
+          data: @resource.as_json(except: %i[tokens created_at updated_at]),
           override_proof: OVERRIDE_PROOF
         }
       else
