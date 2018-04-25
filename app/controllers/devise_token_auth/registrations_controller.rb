51a5e60a075ee3a5bb2b0ebeaee96b58f4e32827
Yonah Wahrhaftig <yonah@golansoftware.com>
2018-04-25 15:33:41 +0300

    return auth headers on register when unconfirmed user does not require confirmation

--- app/controllers/devise_token_auth/registrations_controller.rb
+++ app/controllers/devise_token_auth/registrations_controller.rb
@@ -46,7 +46,9 @@ module DeviseTokenAuth
               client_config: params[:config_name],
               redirect_url: @redirect_url
             })
-          else
+          end
+
+          if !@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?
             # email auth has been bypassed, authenticate user
             @client_id, @token = @resource.create_token
             @resource.save!
