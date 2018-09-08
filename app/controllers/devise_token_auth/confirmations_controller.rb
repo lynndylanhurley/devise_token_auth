724ca08a7f29b74d2758880722b6b8a1d5ebab1d
Maysam Torabi <maysam@codehospital.com>
2018-09-09 04:05:43 +0430

    confirmation should redirect to default_confirm_success_url by default (#1091)

--- app/controllers/devise_token_auth/confirmations_controller.rb
+++ app/controllers/devise_token_auth/confirmations_controller.rb
@@ -22,8 +22,15 @@ module DeviseTokenAuth
         redirect_headers = build_redirect_headers(token,
                                                   client_id,
                                                   redirect_header_options)
-        redirect_to(@resource.build_auth_url(params[:redirect_url],
-                                             redirect_headers))
+
+        # give redirect value from params priority
+        @redirect_url = params[:redirect_url]
+
+        # fall back to default value if provided
+        @redirect_url ||= DeviseTokenAuth.default_confirm_success_url
+
+
+        redirect_to(@resource.build_auth_url(@redirect_url, redirect_headers))
       else
         raise ActionController::RoutingError, 'Not Found'
       end
