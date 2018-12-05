52a0b0eed50617485c2e064d4cf37b07829485c3
Konstantin <kostya1917@gmail.com>
2018-12-05 22:20:00 +0300

    Drop Devise Trackable module

--- test/dummy/app/active_record/unconfirmable_user.rb
+++ test/dummy/app/active_record/unconfirmable_user.rb
@@ -3,7 +3,7 @@
 class UnconfirmableUser < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
+         :recoverable, :rememberable,
          :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
 end
--- test/models/only_email_user_test.rb
+++ test/models/only_email_user_test.rb
@@ -4,14 +4,6 @@ require 'test_helper'
 
 class OnlyEmailUserTest < ActiveSupport::TestCase
   describe OnlyEmailUser do
-    test 'trackable is disabled' do
-      refute OnlyEmailUser.method_defined?(:sign_in_count)
-      refute OnlyEmailUser.method_defined?(:current_sign_in_at)
-      refute OnlyEmailUser.method_defined?(:last_sign_in_at)
-      refute OnlyEmailUser.method_defined?(:current_sign_in_ip)
-      refute OnlyEmailUser.method_defined?(:last_sign_in_ip)
-    end
-
     test 'confirmable is disabled' do
       refute OnlyEmailUser.method_defined?(:confirmation_token)
       refute OnlyEmailUser.method_defined?(:confirmed_at)
