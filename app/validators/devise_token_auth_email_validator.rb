57cefa4ca6706602b091071eadafaa69b00004dd
ihatov08 <ihatov08@gmail.com>
2018-12-07 14:55:37 +0900

    Delete namespece and fix file name change to prevent override behavior of the default email validator

--- app/validators/devise_token_auth_email_validator.rb
+++ app/validators/devise_token_auth_email_validator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DeviseTokenAuth::EmailValidator < ActiveModel::EachValidator
+class DeviseTokenAuthEmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
       record.errors[attribute] << email_invalid_message
