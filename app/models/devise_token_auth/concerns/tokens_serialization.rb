f929fb458012a91474f92cd948739cea98bcb419
Konstantin <18617428+dks17@users.noreply.github.com>
2019-03-23 23:22:34 +0300

    Tokens serialization (#1250)

---
+++ app/models/devise_token_auth/concerns/tokens_serialization.rb
@@ -0,0 +1,19 @@
+module DeviseTokenAuth::TokensSerialization
+  # Serialization hash to json
+  def self.dump(object)
+    object.each_value(&:compact!) unless object.nil?
+    JSON.generate(object)
+  end
+
+  # Deserialization json to hash
+  def self.load(json)
+    case json
+    when String
+      JSON.parse(json)
+    when NilClass
+      {}
+    else
+      json
+    end
+  end
+end
