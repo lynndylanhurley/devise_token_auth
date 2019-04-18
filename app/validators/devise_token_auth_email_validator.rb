2a3c55d9e2d7df4ece2fb4504c3f96288e0e6ef5
Guillermo Kuster <hiperguille10@gmail.com>
2019-04-18 17:44:32 -0300

    Fix Style/GuardClause and Metrics/BlockLength rubocop warnings

--- app/validators/devise_token_auth_email_validator.rb
+++ app/validators/devise_token_auth_email_validator.rb
@@ -2,9 +2,9 @@
 
 class DeviseTokenAuthEmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << email_invalid_message
-    end
+    return if value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+    record.errors[attribute] << email_invalid_message
   end
 
   private
--- test/dummy/app/models/concerns/favorite_color.rb
+++ test/dummy/app/models/concerns/favorite_color.rb
@@ -7,13 +7,12 @@ module FavoriteColor
   end
 
   def ensure_correct_favorite_color
-    if favorite_color && (favorite_color != '')
-      unless ApplicationHelper::COLOR_NAMES.any?{ |s| s.casecmp(favorite_color)==0 }
-        matches = ApplicationHelper::COLOR_SEARCH.search(favorite_color)
-        closest_match = matches.last[:string]
-        second_closest_match = matches[-2][:string]
-        errors.add(:favorite_color, "We've never heard of the color \"#{favorite_color}\". Did you mean \"#{closest_match}\"? Or perhaps \"#{second_closest_match}\"?")
-      end
-    end
+    return if !(favorite_color && (favorite_color != '')) || 
+              ApplicationHelper::COLOR_NAMES.any?{ |s| s.casecmp(favorite_color).zero? }
+
+    matches = ApplicationHelper::COLOR_SEARCH.search(favorite_color)
+    closest_match = matches.last[:string]
+    second_closest_match = matches[-2][:string]
+    errors.add(:favorite_color, "We've never heard of the color \"#{favorite_color}\". Did you mean \"#{closest_match}\"? Or perhaps \"#{second_closest_match}\"?")
   end
 end
