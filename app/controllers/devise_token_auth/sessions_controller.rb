92ff2a0b09319df0ba2651af8de381cefc81d8e5
Guillermo Kuster <hiperguille10@gmail.com>
2019-04-18 19:10:18 -0300

    Fix Layout/EmptyLineAfterGuardClause rubocop warnings

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -26,6 +26,7 @@ module DeviseTokenAuth
         if (@resource.respond_to?(:valid_for_authentication?) && !@resource.valid_for_authentication? { valid_password }) || !valid_password
           return render_create_error_bad_credentials
         end
+
         @client_id, @token = @resource.create_token
         @resource.save
 
