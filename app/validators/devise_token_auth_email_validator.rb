e46bc6f37aa6412b2ec9f7170c6a63e74a5ca14d
Guillermo Kuster <hiperguille10@gmail.com>
2019-04-18 17:44:32 -0300

    Fix Style/GuardClause and Metrics/BlockLength rubocop warnings

--- app/validators/devise_token_auth_email_validator.rb
+++ app/validators/devise_token_auth_email_validator.rb
@@ -2,9 +2,8 @@
 
 class DeviseTokenAuthEmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << email_invalid_message
-    end
+    record.errors[attribute] << email_invalid_message unless
+    value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   end
 
   private
