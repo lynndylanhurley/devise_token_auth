334813b766406e4cb9ef71916e338661045350d8
Mark Li <mark@philosophie.is>
2019-09-12 15:08:53 -0700

    Remove do

--- app/controllers/devise_token_auth/concerns/set_user_by_token.rb
+++ app/controllers/devise_token_auth/concerns/set_user_by_token.rb
@@ -119,7 +119,7 @@ module DeviseTokenAuth::Concerns::SetUserByToken
     # At this point restore the attributes so we can obtain the lock
     # otherwise an exception is thrown while trying to obtain the lock
     # with unsaved data on the resource.
-    unless const_defined?('Mongoid') do
+    unless const_defined?('Mongoid')
       @resource.restore_attributes
     end
     
