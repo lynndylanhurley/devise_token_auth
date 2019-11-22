612af3c8ba59de230b04216d8a68e3e28695e369
Jan <mail@jezekjan.com>
2019-11-22 15:38:08 +0100

    Please merge again (#1350)
    Fix space in defined?

--- app/models/devise_token_auth/concerns/user.rb
+++ app/models/devise_token_auth/concerns/user.rb
@@ -218,7 +218,7 @@ module DeviseTokenAuth::Concerns::User
   end
 
   def should_remove_tokens_after_password_reset?
-    if Rails::VERSION::MAJOR <= 5 || defined?(Mongoid)
+    if Rails::VERSION::MAJOR <= 5 ||defined?('Mongoid')
       encrypted_password_changed? &&
         DeviseTokenAuth.remove_tokens_after_password_reset
     else
