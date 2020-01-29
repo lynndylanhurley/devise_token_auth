cf95469aa52dfde24f67cf50e5d8ef5729828a74
Luisa Lima <luisalima@users.noreply.github.com>
2020-01-29 13:46:51 +0000

    Add paranoid mode to confirmations controller

--- app/controllers/devise_token_auth/confirmations_controller.rb
+++ app/controllers/devise_token_auth/confirmations_controller.rb
@@ -54,14 +54,23 @@ module DeviseTokenAuth
     end
 
     def render_create_success
+      message = if Devise.paranoid
+                  I18n.t('devise_token_auth.confirmations.sended_paranoid')
+                else
+                  I18n.t('devise_token_auth.confirmations.sended', email: @email)
+                end
       render json: {
-          success: true,
-          message: I18n.t('devise_token_auth.confirmations.sended', email: @email)
-      }
+               success: true,
+               message: message
+             }
     end
 
     def render_not_found_error
-      render_error(404, I18n.t('devise_token_auth.confirmations.user_not_found', email: @email))
+      if Devise.paranoid
+        render_error(404, I18n.t('devise_token_auth.confirmations.sended_paranoid'))
+      else
+        render_error(404, I18n.t('devise_token_auth.confirmations.user_not_found', email: @email))
+      end
     end
 
     private
