aa415f4cd470541265b7db2a08a9c1be31261877
Hai Phan Nguyen <pnghai@gmail.com>
2020-06-05 15:54:38 +0700

    Update user_omniauth_callbacks.rb

--- app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
+++ app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
@@ -23,7 +23,7 @@ module DeviseTokenAuth::Concerns::UserOmniauthCallbacks
   end
 
   def sync_uid
-    if devise_modules.include?(:confirmable) 
+    if devise_modules.include?(:confirmable) && !@bypass_confirmation_postpone
       return if postpone_email_change?
     end
     self.uid = email if email_provider?
