2284e741d455753e00ee6669f8ddfb10ee0b632e
Micah Gideon Modell <micah.modell@gmail.com>
2020-10-17 09:35:38 -0400

    solved the problem of multi-email confirm with oauth

--- app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
+++ app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
@@ -8,8 +8,12 @@ module DeviseTokenAuth::Concerns::UserOmniauthCallbacks
     validates :email, :devise_token_auth_email => true, allow_nil: true, allow_blank: true, if: :email_provider?
     validates_presence_of :uid, unless: :email_provider?
 
-    # only validate unique emails among email registration users
-    validates :email, uniqueness: { case_sensitive: false, scope: :provider }, on: :create, if: :email_provider?
+    # Provide support for devise-multi_email - they implement case_sensitive
+    # and maintain uniqueness themselves.
+    unless Gem.loaded_specs[ 'devise-multi_email' ]
+      # only validate unique emails among email registration users
+      validates :email, uniqueness: { case_sensitive: false, scope: :provider }, on: :create, if: :email_provider?
+    end
 
     # keep uid in sync with email
     before_save :sync_uid
