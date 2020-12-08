f4b0a912052f8439f87bebcb6aeb128f6b478c16
‮Artem Kozaev <artemkozaev@gmail.com>
2020-12-08 22:58:00 +0300

    Add email validation errors via add (#1445)

--- app/validators/devise_token_auth_email_validator.rb
+++ app/validators/devise_token_auth_email_validator.rb
@@ -3,7 +3,7 @@
 class DeviseTokenAuthEmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << email_invalid_message
+      record.errors.add(attribute, email_invalid_message)
     end
   end
 
