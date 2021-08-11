05c870d0763efe2f5bf7566d05a123f534a590b3
muratiger <theboxer019690@gmail.com>
2021-08-11 21:46:06 +0900

    Turn email validation process into class method (#1494)

--- app/validators/devise_token_auth_email_validator.rb
+++ app/validators/devise_token_auth_email_validator.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class DeviseTokenAuthEmailValidator < ActiveModel::EachValidator
+  EMAIL_REGEXP = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  class << self
+    def validate?(email)
+      email =~ EMAIL_REGEXP
+    end
+  end
+
   def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    unless DeviseTokenAuthEmailValidator.validate?(value)
       record.errors.add(attribute, email_invalid_message)
     end
   end
